4ad52891d63aca6b985123d98c11f03fd8594875
elnur.ahmadov <elnur.ahmadov@cybernet.az>
2026-02-03 11:38:52 +0400

    Removed Slf4j

--- src/main/java/az/ingress/service/concrete/RecommendationServiceHandler.java
+++ src/main/java/az/ingress/service/concrete/RecommendationServiceHandler.java
@@ -6,14 +6,12 @@ import az.ingress.model.policy.ActionWeightProvider;
 import az.ingress.model.response.RecommendationResponse;
 import az.ingress.service.abstraction.RecommendationService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RecommendationServiceHandler implements RecommendationService {
